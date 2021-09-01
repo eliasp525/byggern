913e04b7447a805a03389ba0c61e3591c659a63a
Adrian Stokdal Opheim <adrian.s.opheim@gmail.com>
2021-09-01 13:57:06 +0200

    Added main.c and Makefile

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <avr/io.h>
+#define F_CPU 4915200
+#include <util/delay.h>
+
+
+#define set_bit(reg, bit) (reg |= (1 << bit))
+#define clear_bit(reg, bit) (reg  &= ~(1<<bit))
+#define test_bit(reg, bit) (reg & (1 << bit))
+//#define loop_until_bit_is_set(reg, bit) while(!test_bit(reg ,bit))
+//#define loop_until_bit_is_clear(reg, bit) while(test_bit(reg ,bit))
+
+int main(){
+    DDRA = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        set_bit(PORTA,1);
+        _delay_ms(100);
+        clear_bit(PORTA,1);
+        _delay_ms(100);
+    }
+    
+
+    return 0;
+}
