f4af8cfff72a5b1d62c5ee8a8ec58ae23406ae72
Adrian Stokdal Opheim <adrianop@ntnu.no>
2021-10-20 17:27:52 +0200

    last commit 20oct, works very ish, but joystick position not sent.

--- node_2/main.c
+++ node_2/main.c
@@ -14,6 +14,75 @@
 
 #include "sam.h"
 
+#define DEBUG_INTERRUPT 1
+
+CAN_MESSAGE message;
+
+/**
+ * \brief CAN0 Interrupt handler for RX, TX and bus error interrupts
+ *
+ * \param void
+ *
+ * \retval 
+ */
+void CAN0_Handler( void )
+{
+	if(DEBUG_INTERRUPT)printf("CAN0 interrupt\n\r");
+	char can_sr = CAN0->CAN_SR; 
+	
+	//RX interrupt
+	if(can_sr & (CAN_SR_MB1 | CAN_SR_MB2) )//Only mailbox 1 and 2 specified for receiving
+	{
+		
+		if(can_sr & CAN_SR_MB1)  //Mailbox 1 event
+		{
+
+			can_receive(&message, 1);
+		}
+		else if(can_sr & CAN_SR_MB2) //Mailbox 2 event
+		{
+			can_receive(&message, 2);
+		}
+		else
+		{
+			printf("CAN0 message arrived in non-used mailbox\n\r");
+		}
+
+		if(DEBUG_INTERRUPT)printf("message id: %d\n\r", message.id);
+		if(DEBUG_INTERRUPT)printf("message data length: %d\n\r", message.data_length);
+		for (int i = 0; i < message.data_length; i++)
+		{
+			if(DEBUG_INTERRUPT)printf("%d ", message.data[i]);
+		}
+		if(DEBUG_INTERRUPT)printf("\n\r");
+
+		can_send(&message, 0);
+	}
+	
+	if(can_sr & CAN_SR_MB0)
+	{
+		if(DEBUG_INTERRUPT) printf("CAN0 MB0 ready to send \n\r");
+		
+	//Disable interrupt
+		CAN0->CAN_IDR = CAN_IER_MB0;
+
+	}
+
+	if(can_sr & CAN_SR_ERRP)
+	{
+		if(DEBUG_INTERRUPT)printf("CAN0 ERRP error\n\r");
+
+	}
+	if(can_sr & CAN_SR_TOVF)
+	{
+		if(DEBUG_INTERRUPT)printf("CAN0 timer overflow\n\r");
+
+	}
+	
+	NVIC_ClearPendingIRQ(ID_CAN0);
+	//sei();*/
+}
+
 
 int main(void)
 {
@@ -22,9 +91,9 @@ int main(void)
 
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	
-	can_default_init();
 	
 	configure_uart();
+	can_default_init();
 	printf("System initialized.\n\r");
 	PMC->PMC_PCER0 = PMC_PCER0_PID11;
 	PIOA->PIO_PER = (PIO_PA19 | PIO_PA20 | PIO_PA14);
@@ -37,11 +106,11 @@ int main(void)
 	
 	//char rec_data[8] = "";
 	//CAN_MESSAGE rec_msg = {.data_length = 8, .id = 1, .data = &rec_data};
-	
-	
-	printf("System initialized.\n\r");
     while (1) 
     {
-		//CAN0_Handler();
+		if(message.id == 69){
+			printf("x_pos: %d, y_pos %d\r\n", message.data[1], message.data[3]);
+		}
+		
     }
 }
