4e5a6b668c1c92d1883cde4d3751ded955e27ee0
Olvess <olufelgrande95@gmail.com>
2021-10-20 17:14:25 +0200

    fixed stuff

--- node_1/main.c
+++ node_1/main.c
@@ -141,7 +141,7 @@ int main() {
         //can_msg message = {.id = 1, .data = str, .len = 7};
 
         //can_send_msg(message);
-        send_joystick_x_y(position, bias)
+        send_joystick_x_y(position, bias);
         //printf("Sending message: %s\r\n", message.data);
         _delay_ms(5000);
 
