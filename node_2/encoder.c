1abf68e61aaba78bac8f5e86b30c1007f81bd1e8
Adrian Stokdal Opheim <adrian.s.opheim@gmail.com>
2021-11-03 18:50:59 +0100

    Encoder driver and delay-function with sysTick ready for testing

---
+++ node_2/encoder.c
@@ -0,0 +1,43 @@
+#include "encoder.h"
+#include <util/delay.h>
+
+void encoder_init(){
+    PMC->PMC_PCER0 |= PMC_PCER0_PID13; //Enable clock for PIOC
+    PMC->PMC_PCER0 |= PMC_PCER0_PID14; //Enable clock for PIOD
+    
+    PIOD->PIO_PER = (ENCODER_OE | ENCODER_RST | ENCODER_SEL); // Enable pins
+    PIOC->PIO_PER = (ENCODER_DO0 | ENCODER_DO1 | ENCODER_DO2 | ENCODER_DO3 | ENCODER_DO4 | ENCODER_DO5 | ENCODER_DO6 | ENCODER_DO7); // Enable parallell data pins
+
+    PIOD->PIO_OER = (ENCODER_OE | ENCODER_RST | ENCODER_SEL); // Set encoder control pins to outputs.
+    PIOC->PIO_ODR = (ENCODER_DO0 | ENCODER_DO1 | ENCODER_DO2 | ENCODER_DO3 | ENCODER_DO4 | ENCODER_DO5 | ENCODER_DO6 | ENCODER_DO7); // Set encoder parallel data pins to inputs
+
+    PIOD->PIO_CODR = ENCODER_RST;
+    PIOD->PIO_SODR = ENCODER_RST; //Toggle reset of encoder in init.
+}
+
+
+
+void encoder_read(uint32_t encoder_counter){
+    PIOD->PIO_CODR = (ENCODER_OE | ENCODER_SEL); // Set !OE low to enable output of encoder and set SEL low to get high byte
+    _delay_us(20);
+    encoder_counter = ((PIOC->PIO_PDSR & ENCODER_DATA_Msk) << 8); //Read high-byte of encoder counter
+    PIOC->PIO_SODR = 1 << ENCODER_SEL; //Set SEL high to get low byte of encoder counter
+    _delay_us(20);
+    encoder_counter |= (PIOC->PIO_PDSR & ENCODER_DATA_Msk); //Read low-byte of encoder counter
+    PIOD->PIO_CODR = ENCODER_RST;
+    PIOD->PIO_SODR = ENCODER_RST; //pulse reset of encoder.
+    PIOD->PIO_SODR = ENCODER_OE; //Disable output from encoder.
+}
+
+void _delay_us(uint32_t time_us){
+    //Setup sysTick for delay
+    SysTick->CTRL = sysTick->CTRL & !SysTick_CTRL_ENABLE_Msk; // Disables counter
+
+    SysTick-> LOAD = 84*time_us; //Should reload after time_us. 84 clock cycles per us with 84 Mhz.
+    SysTick->CTRL |= (1 << SysTick_CTRL_CLKSOURCE_Pos)
+
+    SysTick->CTRL |= (1 << SysTick_CTRL_ENABLE_Pos); // Enables counter to count down from value in SysTick->LOAD
+    while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)) // Wait until COUNTFLAG has been set.
+
+    SysTick->CTRL = sysTick->CTRL & !(SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_COUNTFLAG_Msk); // Disables counter and clears COUNTFLAG
+}
---
+++ node_2/encoder.h
@@ -0,0 +1,26 @@
+
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include "sam.h"
+#include <util/delay.h>
+
+// PIN-definitions
+#define ENCODER_OE PIO_PD0 // active low Output Enable of encoder data
+#define ENCODER_RST PIO_PD1 // active low reset of encoder
+#define ENCODER_SEL PIO_PD2 // Select low or high byte of encoder output
+
+#define ENCODER_DO0 PIO_PC1 // Parallell data bus bits
+#define ENCODER_DO1 PIO_PC2
+#define ENCODER_DO2 PIO_PC3
+#define ENCODER_DO3 PIO_PC4
+#define ENCODER_DO4 PIO_PC5
+#define ENCODER_DO5 PIO_PC6
+#define ENCODER_DO6 PIO_PC7
+#define ENCODER_DO7 PIO_PC8
+
+#define ENCODER_DATA_Msk 0x1FE; // To read only PC1-PC8 for encoder data  
+
+void encoder_init();
+void encoder_read(uint32_t encoder_counter);
+void _delay_us(uint32_t time_us);
